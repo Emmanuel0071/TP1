f8b140c9a0f6700232843471cc6defa19585c651
Amaury <amaury.gaborit17@gmail.com>
2024-12-09 17:13:40 +0100

    clients

--- client.cpp
+++ client.cpp
@@ -1,6 +1,6 @@
 #include "client.h"
 
-Client::Client(std::vector<double> _ISBNlist, std::string _firstname, std::string _lastname,std::string _idClient)
+Client::Client(const std::vector<std::string>& _ISBNList, std::string _firstname, std::string _lastname,std::string _idClient)
     : ISBNList(_ISBNList), firstname(_firstname), lastname(_lastname), idClient(_idClient) {}
 
 const std::string Client::getId() const {
--- client.h
+++ client.h
@@ -9,13 +9,13 @@ class Client {
 
     public:
 
-        Client(std::vector<double> _ISBNList, std::string _firstname, std::string _lastname,std::string _idClient);
+        Client(const std::vector<std::string>& _ISBNList, std::string _firstname, std::string _lastname,std::string _idClient);
 
         const std::string getId() const;
 
     private:
 
-        std::vector<double> ISBNList;
+        std::vector<std::string> ISBNList;
         std::string firstname;
         std::string lastname;
         std::string idClient;
