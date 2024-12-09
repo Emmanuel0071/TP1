da46eba25cf39bb72d936a56b25e420183ff73c1
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-09 15:39:22 +0100

    modif idlecteur+emprunt

---
+++ Borrow.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Date.h"
+
+
+class Brorrow {
+    Public:
+        Borrow(Date date,double ISBN,sdt::string idlecteur);
+    Private:
+        Date _date;
+        double _ISBN;
+        sdt::string _idlecteur;
+}
