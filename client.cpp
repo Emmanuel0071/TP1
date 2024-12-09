c3501446c7367cfed4298019e7dfcb725d3f2a2d
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-09 15:12:26 +0100

    client.cpp creation

---
+++ client.cpp
@@ -0,0 +1,3 @@
+#include "client.h"
+
+Client::Client(std::vector <double> ISBNlist, std::string firstname, std::string lastname,std::string id): _ISBNlist(ISBNlist),_firstname(firstname),_lastname(lastname),_id(id);
