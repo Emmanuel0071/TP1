fb2c2fee3fac07ad915ce54b8a8ae990bc81b7ef
Amaury <amaury.gaborit17@gmail.com>
2024-12-09 16:25:16 +0100

    Add Library Class

---
+++ library.cpp
@@ -0,0 +1,24 @@
+#include "Book.h"
+#include "client.h"
+#include "borrow.h"
+
+class Library {
+private:
+    std::vector<Client> clients;
+    std::vector<Book> books;
+    std::vector<Borrow> borrows;
+
+public:
+    Library();
+
+    const std::vector<Client>& getClients() const;
+    const std::vector<Book>& getBooks() const;
+    const std::vector<Borrow>& getBorrows() const;
+
+    void addClient(const Client& client);
+    void addBook(const Book& book);
+    void addBorrow(const Borrow& book);
+
+    void borrowing(const Client& client, const Book& book);
+
+}
