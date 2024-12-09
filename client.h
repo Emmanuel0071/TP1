4b4c46cd4a9e6a61e2330a68d1606e5903664992
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-09 15:26:17 +0100

    idlecteur

--- client.h
+++ client.h
@@ -7,14 +7,14 @@ class Client {
 
     public:
 
-        Client(std::vector <double> ISBNlist, std::string firstname, std::string lastname,std::string id);
+        Client(std::vector <double> ISBNlist, std::string firstname, std::string lastname,std::string idlecteur);
 
     private:
 
         std::vector<double> _ISBNlist;
         std::string _firstname;
         std::string _lastname;
-        std::string _id;
+        std::string _idlecteur;
 
     
 
