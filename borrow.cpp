bd785ac357386d9ce7a90b1e1d34563d60ccc99d
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-26 18:06:00 +0100

    rajoue de getter et setter sur borrow(emprunt)

--- borrow.cpp
+++ borrow.cpp
@@ -1,8 +1,27 @@
 #include "borrow.h"
+#include "Date.h"
 
-Borrow::Borrow(const std::string& _idClient, const std::string& _ISBN, Date _date)
-    : idClient(_idClient), ISBN(_ISBN), date(_date) {}
+Borrow::Borrow(const std::string& idClient, const std::string& ISBN, Date date)
+    : _idClient(idClient), _ISBN(ISBN), _date(date) {}
 
 const std::string& Borrow::getISBN() const {
-    return ISBN;
+    return _ISBN;
+}
+const Date& Borrow::getDate() const {
+    return _date;
+}
+const std::string& Borrow::getIdClient() const {
+    return _idClient;
+}   
+
+void Borrow::setISBN(const std::string& newISBN){
+    _ISBN = newISBN;
+
+}
+
+void Borrow::setIdClient(const std::string& newIdClient){
+    _idClient = newIdClient;
+}
+void Borrow::setDate(const Date& newDate){
+    _date=newDate;
 }
--- borrow.h
+++ borrow.h
@@ -7,13 +7,19 @@
 
 class Borrow {
 private:
-    std::string idClient;
-    std::string ISBN;
-    Date date;
+    std::string _idClient;
+    std::string _ISBN;
+    Date _date;
 
 public:
-    Borrow(const std::string& _idClient, const std::string& _ISBN, Date _date);
+    Borrow(const std::string& idClient, const std::string& ISBN, Date date);
 
     const std::string& getISBN() const;
+    const Date& getDate() const;
+    const std::string& getIdClient() const;
 
+
+    void setISBN (const std::string& NewISBN);
+    void setDate (const Date& NewDate);
+    void setIdClient (const std::string& NewIdClient);
 };
