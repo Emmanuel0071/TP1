ae208f3e551a475bc864891323c98e2f031c055e
Emmanuel <Emmanuel_Peyronnet@etu.u-bourgogne.fr>
2024-12-26 18:47:35 +0100

    Refactor Client class: add getters and setters, update constructor parameters

--- client.cpp
+++ client.cpp
@@ -1,8 +1,29 @@
 #include "client.h"
 
-Client::Client(const std::vector<std::string>& _ISBNList, std::string _firstname, std::string _lastname,std::string _idClient)
-    : ISBNList(_ISBNList), firstname(_firstname), lastname(_lastname), idClient(_idClient) {}
+Client::Client(const std::vector<std::string>& ISBNList, std::string firstname, std::string lastname,std::string idClient)
+    : _ISBNList(ISBNList), _firstname(firstname), _lastname(lastname), _idClient(idClient) {}
 
-const std::string Client::getId() const {
-    return idClient;
+const std::string& Client::getIdClient() const{
+    return _idClient;
+}
+const std::string& Client::getFirstname() const{
+    return _firstname;
+}
+const std::vector<std::string>& Client::getISBNList() const {
+    return _ISBNList;
+}
+
+void Client::setIdClient(std::string& newIdClient){
+    _idClient = newIdClient;
+}
+
+void Client::setfirstname(std::string& newFirstname){
+    _firstname=newFirstname;
+}
+void Client::setlastname(std::string& newLastname)
+{
+    _lastname=newLastname;
+}
+void Client::setISBNList(std::vector<std::string>& newISBNList){
+    _ISBNList=newISBNList;
 }
--- client.h
+++ client.h
@@ -9,18 +9,23 @@ class Client {
 
     public:
 
-        Client(const std::vector<std::string>& _ISBNList, std::string _firstname, std::string _lastname,std::string _idClient);
+        Client(const std::vector<std::string>& ISBNList, std::string firstname, std::string lastname,std::string idClient);
 
-        const std::string getId() const;
+        const std::string& getIdClient() const;
+        const std::string& getFirstname() const;
+        const std::string& getLastname() const;
+        const std::vector<std::string>& getISBNList() const;
+
+        void setIdClient(std::string& newIdClient);
+        void setfirstname(std::string& newFirstname);
+        void setlastname(std::string& newLastname);
+        void setISBNList(std::vector<std::string>& newISBNList);
 
     private:
 
-        std::vector<std::string> ISBNList;
-        std::string firstname;
-        std::string lastname;
-        std::string idClient;
-
-    
-
+        std::vector<std::string> _ISBNList;
+        std::string _firstname;
+        std::string _lastname;
+        std::string _idClient;
 
 };
